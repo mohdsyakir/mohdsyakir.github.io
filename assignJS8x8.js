6704f407ed710425f2cf1a73be8c8f921f834423
syakir webDev <syakir@web.dev>
2020-11-08 19:22:10 +0800

    js 8x8

---
+++ assignJS8x8.js
@@ -0,0 +1,31 @@
+var temp = "";
+var isEvenRow = false;
+var isEvenCol = false;
+
+for (i=1; i <= 7; i++) {
+ temp = "";
+ if (i % 2 == 0) {
+  isEvenRow = true;
+ } else {
+  isEvenRow = false;
+ }
+
+for (j=1; j <= 9; j++) {
+ if (j % 2 == 0) {
+  isEvenCol = true;
+ } else {
+  isEvenCol = false;
+ }
+
+ if (isEvenRow == true && isEvenCol == true) {
+  temp += "#";
+ } else if (isEvenRow == true && isEvenCol == false) {
+  temp += " ";
+ } else if (isEvenRow == false && isEvenCol == false) {
+  temp += "#";
+ } else if (isEvenRow == false && isEvenCol == true) {
+  temp += " ";
+ }
+}
+console.log(temp);
+}
